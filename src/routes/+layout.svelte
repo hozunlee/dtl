09377b7f675ccb737eb07edbdbad883b3c1d730e
hozunlee <ho2yahh@gmail.com>
2023-03-27 16:27:47 +0900

    fix : logout logic 수정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,29 @@
 
 	export let data: LayoutData;
 
-	let isLogin = true;
+	let isLogin = false;
 
 	$: ({ supabase } = data);
 
 	const handleLogout = async () => {
 		const { error } = await supabase.auth.signOut();
+
+		if (error) {
+			console.error(error);
+			return alert('로그아웃에서 에러가 발생!!');
+		}
 		isLogin = false;
 		alert('로그아웃됨 바이');
 		goto('/');
 	};
 
 	onMount(async () => {
+		const {
+			data: { session }
+		} = await supabase.auth.getSession();
+
+		if (session) isLogin = true;
+
 		const { data } = supabase.auth.onAuthStateChange(() => {
 			invalidate('supabase:auth');
 		});
